c532e60a93711afa6b07430e03cf76320de16e5c
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2025-11-05 09:25:34 -0500

    update

--- Game/Levels/L15PsetIntro.lean
+++ Game/Levels/L15PsetIntro.lean
@@ -6,11 +6,16 @@ Title "Pset 15"
 Introduction "
 # Problem Set 15: Solve these problems in Natural Language:
 
-$\\# 1)$ Let $a_0 := \\sqrt2$, and $a_{n+1}:=\\sqrt{2+\\sqrt{a_n}}$. Prove that $a_n < 2$ hols for all $n$, and that $a_n$ converges.
+$\\# 1)$ Let $a_0 := \\sqrt2$, and $a_{n+1}:=\\sqrt{2+\\sqrt{a_n}}$. Prove that $a_n < 2$ holds for all $n$, and that $a_n$ converges.
 
 $\\# 2)$ Calculate $\\lim_{n\\to\\infty}\\left(\\sqrt{n^2+n}-n\\right)$ rigorously.
 
-$\\# 3)$
+$\\# 3)$ Show that if a sequence `a : ℕ → ℝ` is convergent,
+ then the \"Cesaro-average\" sequence given by:
 
+ `b n = (a 0 + a 1 + ... + a (n-1)) / n`
+
+converges, and to the same limit. Give an example of a sequence `a : ℕ → ℝ` which
+does *not* converge, but for which the Cesaro average sequence does converge.
 
 "
